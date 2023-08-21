b3bb85c3bf11ffaca5260dc715c1e280bc89e154
othAs-dev <othmane.developer@gmail.com>
2023-08-21 12:27:25 +0200

    chore: deleting module which are not essential to the app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http'; 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonModule} from 'primeng/button';
-import { MenuModule } from 'primeng/menu';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatIconModule } from '@angular/material/icon';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AutoCompleteModule } from 'primeng/autocomplete';
 
 
 @NgModule({
@@ -21,19 +12,9 @@ import { AutoCompleteModule } from 'primeng/autocomplete';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    ButtonModule,
     HttpClientModule,
     AppRoutingModule,
-    MenuModule,
-    FormsModule,
-    MatAutocompleteModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
-    ReactiveFormsModule,
     RouterModule,
-    AutoCompleteModule
-
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
