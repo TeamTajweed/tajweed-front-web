692a19cde5b20ee101bd19c2ba5c78ecc40278cf
Pseudonyme <43927551+Pseudonyme7@users.noreply.github.com>
2023-09-19 09:15:20 +0200

    Update statistiques.component.ts

--- src/app/_teacher/statistiques/statistiques.component.ts
+++ src/app/_teacher/statistiques/statistiques.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import {NavbarComponent} from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-statistiques',
   templateUrl: './statistiques.component.html',
   styleUrls: ['./statistiques.component.scss'],
   imports: [
-    NavbarComponent
   ],
   standalone: true
 })
