864c4fc311aaabec653973476907260c73cdfbae
Guillaume Martinez <guillaume.m.developer@gmail.com>
2023-10-03 09:05:36 +0200

    communication success

--- src/app/_shared/landing/landing.component.ts
+++ src/app/_shared/landing/landing.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TeacherService } from '../../core/services/teacher.service';
-import { Teacher } from '../../core/models/teacher.model';
 
 
 @Component({
@@ -9,23 +7,10 @@ import { Teacher } from '../../core/models/teacher.model';
     styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit{
-    teachers: Teacher[] = [];
-
-    constructor(private teacherService: TeacherService) { }
+   
+    constructor() { }
   
     ngOnInit() {
-      this.getTeachers();
     }
-  
-    getTeachers() {
-      this.teacherService.getTeachers().subscribe(
-        teachers => {
-          this.teachers = teachers;
-          console.log(this.teachers); // Affichez les teachers dans la console
-        },
-        error => {
-          console.error('Une erreur s\'est produite lors de la récupération des étudiants:', error);
-        }
-      );
-    }
+
 }
--- src/app/core/services/institute.service.ts
+++ src/app/core/services/institute.service.ts
@@ -4,13 +4,14 @@ import { Observable } from 'rxjs';
 import { Institute } from '../models/institute.model';
 import { HttpHeaders } from '@angular/common/http';
 
-// à finir controller coter back
+
 @Injectable({
   providedIn: 'root'
 })
 export class InstituteService {
   readonly API_URL = "http://localhost:3000";
   readonly ENDPOINT_INSTITUTES = "/institute";
+
 
   constructor(private httpClient: HttpClient) { }
 
