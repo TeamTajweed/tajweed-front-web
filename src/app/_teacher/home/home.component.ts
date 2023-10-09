de09b0bfdd06acf666b95f36ee8f1a77fc7bb0b7
Guillaume Martinez <guillaume.m.developer@gmail.com>
2023-10-09 11:11:07 +0200

    fix bug home page and added features to the sourates table.

--- src/app/_teacher/home/home.component.ts
+++ src/app/_teacher/home/home.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit, ViewChild, ElementRef, Renderer2 } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  Renderer2,
+} from "@angular/core";
 import { ValidationComponent } from "../../_teacher/validation/validation.component";
-import { FormsModule } from "@angular/forms";
-import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button';
-import { CommonModule } from '@angular/common';
-import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { QuranService } from '../../core/services/quran.service';
-import { Student } from '../../core/models/student.model';
-import { StudentService } from '../../core/services/student.service';
-import { NotificationService } from '../../core/services/notification.service';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
-import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
-import { Notification as AppNotification } from '../../core/models/notification.model';
+import { Student } from "../../core/models/student.model";
+import { StudentService } from "../../core/services/student.service";
+import { NotificationService } from "../../core/services/notification.service";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
+import { Notification as AppNotification } from "../../core/models/notification.model";
+import { sourates } from "../../_students/homestudents/homestudents.component";
+import { ChangeDetectorRef } from "@angular/core";
 
 interface AutoCompleteCompleteEvent {
   originalEvent: Event;
@@ -26,7 +26,6 @@ interface AutoCompleteCompleteEvent {
   templateUrl: "./home.component.html",
   styleUrls: ["./home.component.scss"],
 })
-
 export class HomeComponent implements OnInit {
   @ViewChild(ValidationComponent) validationComponent!: ValidationComponent;
   students: Student[] = [];
@@ -35,42 +34,98 @@ export class HomeComponent implements OnInit {
   selectedStudentId: string | null = null;
   buttonDisplayed: boolean = false;
   displayStudentCard = false;
-  sidebarWidth: string = '100%';
+  sidebarWidth: string = "100%";
+  phoneticValue: string | null = null;
+  phoneticsForNotifications: { [key: number]: string } = {};
 
-  constructor(private http: HttpClient, private el: ElementRef, private renderer: Renderer2, private studentService: StudentService, private notificationService: NotificationService) {}
+  constructor(
+    private cdr: ChangeDetectorRef,
+    private http: HttpClient,
+    private el: ElementRef,
+    private renderer: Renderer2,
+    private studentService: StudentService,
+    private notificationService: NotificationService
+  ) {}
 
   ngOnInit() {
     this.getStudents();
   }
 
+  //je récupère la liste des étudiants , pour chaque étudiant, je récupère leurs notifications
   getStudents() {
-    this.studentService.getStudents().subscribe(students => {
+    this.studentService.getStudents().subscribe((students) => {
       this.students = students;
-      this.students.forEach(student => {
-        this.getNotificationsByStudentId(student.id).subscribe((notifications: AppNotification[]) => {
-          student.notifications = notifications;
-        });
+
+      this.students.forEach((student) => {
+        this.getNotificationsByStudentId(student.id).subscribe(
+          (notifications: AppNotification[]) => {
+            student.notifications = notifications;
+            this.loadPhoneticsForNotifications();
+            this.cdr.detectChanges();
+          }
+        );
       });
     });
   }
 
-  getNotificationsByStudentId(studentId: string): Observable<AppNotification[]> {
+  // parcourt la liste des étudiants et leurs notifications Pour chaque notification, j'utilise la valeur de la propriété 'surate' pour obtenir une valeur phonétique en appelant 'getPhoneticBySurateId'
+  loadPhoneticsForNotifications(): void {
+    for (let student of this.students) {
+      if (student.notifications) {
+        // S'assurer que les notifications existent
+        for (let notification of student.notifications) {
+          const phoneticValue = this.getPhoneticBySurateId(
+            +notification.surate
+          );
+          if (phoneticValue) {
+            this.phoneticsForNotifications[+notification.surate] =
+              phoneticValue;
+          }
+        }
+      }
+    }
+  }
+
+  // recherche une sourate par son identifiant 'id' dans le tableau 'sourates' dans homeStudents.component.ts
+  getPhoneticBySurateId(id: number): string | null {
+    const surate = sourates.find((s) => s.id === id);
+    return surate ? surate.phonetic : null;
+  }
+
+  //récupère la valeur phonétique 'sourates' dans homeStudents.component.ts
+  getPhoneticForSurate(surateId: number): string {
+    return this.phoneticsForNotifications[surateId];
+  }
+
+  // obtient les notifications pour un étudiant, recherche les valeurs phonétiques correspondantes des sourates, et renvoie un Observable contenant la liste des notifications pour l'étudiant.
+  getNotificationsByStudentId(
+    studentId: string
+  ): Observable<AppNotification[]> {
     const numericStudentId = parseInt(studentId, 10);
-    return this.notificationService.getNotificationsByStudentId(numericStudentId).pipe(
-      tap(notifications => {
-        this.notifications = notifications;
-      })
-    );
+    return this.notificationService
+      .getNotificationsByStudentId(numericStudentId)
+      .pipe(
+        tap((notifications: AppNotification[]) => {
+          const tempPhonetics: { [key: number]: string } = {};
+          notifications.forEach((notification) => {
+            const phoneticValue = this.getPhoneticBySurateId(
+              Number(notification.surate)
+            );
+            if (phoneticValue) {
+              tempPhonetics[Number(notification.surate)] = phoneticValue;
+            }
+          });
+          this.phoneticsForNotifications = tempPhonetics;
+        })
+      );
   }
 
-  // updateSelectedPage(student: Student): void {}
-  updateSelectedPage(student: Student | null): void {
-  }
+  updateSelectedPage(student: Student | null): void {}
 
-
+  //affiche la carte d'étudiant en prenant un objet étudiant en entrée, elle met à jour plusieurs propriétés et affiche la carte d'étudiant,
   showStudentCard(student: any) {
     if (student) {
-      console.log(student)
+      console.log(student);
       this.selectedStudent = student;
       this.selectedStudentId = student.id;
       this.buttonDisplayed = true;
@@ -83,14 +138,14 @@ export class HomeComponent implements OnInit {
       this.buttonDisplayed = true;
     }
   }
-
+  //fonction pour le responsive
   toggleSidebar() {
-    this.sidebarWidth = this.sidebarWidth === '20%' ? '100%' : '20%';
-    const leftSidebar = this.el.nativeElement.querySelector('.left-sidebar');
-    if (this.sidebarWidth === '20%') {
-      this.renderer.setStyle(leftSidebar, 'display', 'none');
+    this.sidebarWidth = this.sidebarWidth === "20%" ? "100%" : "20%";
+    const leftSidebar = this.el.nativeElement.querySelector(".left-sidebar");
+    if (this.sidebarWidth === "20%") {
+      this.renderer.setStyle(leftSidebar, "display", "none");
     } else {
-      this.renderer.setStyle(leftSidebar, 'display', 'block');
+      this.renderer.setStyle(leftSidebar, "display", "block");
     }
   }
 }
--- src/app/_teacher/validation/validation.component.ts
+++ src/app/_teacher/validation/validation.component.ts
@@ -1,14 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { QuranService } from '../../core/services/quran.service';
-import { CommonModule } from '@angular/common';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-import { AudioRecordingService } from '../../core/services/audio.recording.service';
-
+import { Component, Input, OnInit } from "@angular/core";
+import { QuranService } from "../../core/services/quran.service";
+import { DomSanitizer, SafeUrl } from "@angular/platform-browser";
+import { AudioRecordingService } from "../../core/services/audio.recording.service";
 
 @Component({
   selector: "app-validation",
   templateUrl: "./validation.component.html",
-  styleUrls: ["./validation.component.scss"]
+  styleUrls: ["./validation.component.scss"],
 })
 export class ValidationComponent implements OnInit {
   @Input() selectedStudent: any;
@@ -20,12 +18,12 @@ export class ValidationComponent implements OnInit {
   totalSourate: boolean[][] = [];
   selectedStudentId: string | null = null;
   students: any[] = [];
-  
 
   ayah: any = null;
   selectedPage: number = 1;
   selectedVerset: string = "";
   selectedSourateName: string = "";
+  selectedSourate: any = null;
 
   //Audio
   isRecording = false;
@@ -35,27 +33,34 @@ export class ValidationComponent implements OnInit {
   audioList: AudioforTransit[] = [];
   currentVerse: number = 0;
   currentlyPlayingAudio: HTMLAudioElement | null = null;
-  isAudioPlaying: { [key: string]: boolean } = {}; 
+  isAudioPlaying: { [key: string]: boolean } = {};
+  // Propriété pour stocker l'ID de la frame d'animation pour la progress barre
+  private animationFrameId: number | undefined;
+  // tableau pour stocker la vitesse de lecture de chaque élément audio
+  audioPlaybackRates: { [key: string]: number } = {};
 
+  //On représente l'objet de transit d'un audio
 
-
-
-   //On représente l'objet de transit d'un audio
-
-  constructor(private quranService: QuranService,private audioRecordingService: AudioRecordingService,private sanitizer: DomSanitizer) {
-      
-      this.audioRecordingService.recordingFailed().subscribe(() => (this.isRecording = false));
-      this.audioRecordingService.getRecordedTime().subscribe(time => (this.recordedTime = time));
-      this.audioRecordingService.getRecordedBlob().subscribe(data => {
-        this.teste = data;
-        this.blobUrl = this.sanitizer.bypassSecurityTrustUrl(
-          URL.createObjectURL(data.blob)
-        );
-      });
-    }
+  constructor(
+    private quranService: QuranService,
+    private audioRecordingService: AudioRecordingService,
+    private sanitizer: DomSanitizer
+  ) {
+    this.audioRecordingService
+      .recordingFailed()
+      .subscribe(() => (this.isRecording = false));
+    this.audioRecordingService
+      .getRecordedTime()
+      .subscribe((time) => (this.recordedTime = time));
+    this.audioRecordingService.getRecordedBlob().subscribe((data) => {
+      this.teste = data;
+      this.blobUrl = this.sanitizer.bypassSecurityTrustUrl(
+        URL.createObjectURL(data.blob)
+      );
+    });
+  }
   ngOnInit() {
     this.getDataQuran();
-    
   }
 
   getDataQuran() {
@@ -77,24 +82,26 @@ export class ValidationComponent implements OnInit {
   }
   // Initialisation de la fonction pour pouvoir l'importer de home component dans validation component
   showStudentCard(student: any) {}
+
   //fonction pour faire matcher la page saisi par l'étudiant et la page du Qur'an de l'api
   updateSelectedPage(student: any): void {
     if (student.notifications && student.notifications.length > 0) {
       const matchingSourate = this.Coran.find(
         (sourate) => sourate.number === student.notifications[0].surate
       );
-  
+
       if (matchingSourate) {
-        this.selectedPage = matchingSourate.ayahs[0].page;
+        this.selectedSourate = matchingSourate; // Met à jour la sourate sélectionnée
       }
     }
   }
-  
+
   //fonction pour faire matcher le verset débutant l'audio saisi par l'étudiant et les verset du Qur'an de l'api
   updateSelectVerset(student: any): void {
     const matchingVerset = this.Coran.find((sourate: any) =>
       sourate.ayahs.some(
-        (ayah: any) => ayah.numberInSurah === student.notifications[0].verseStart
+        (ayah: any) =>
+          ayah.numberInSurah === student.notifications[0].verseStart
       )
     );
 
@@ -111,8 +118,8 @@ export class ValidationComponent implements OnInit {
       targetElement.scrollIntoView();
     }
   }
-   // Fonctions relatives aux audios
-   startRecording() {
+  // Fonctions relatives aux audios
+  startRecording() {
     if (!this.isRecording) {
       this.isRecording = true;
       this.audioRecordingService.startRecording();
@@ -132,13 +139,13 @@ export class ValidationComponent implements OnInit {
       this.isRecording = false;
     }
   }
-//supprime l'audio en cours d'enregistrement
+  //supprime l'audio en cours d'enregistrement
   clearRecordedData() {
     this.stopRecording();
     this.blobUrl = null;
     this.audioRecordingService.resetTimer();
   }
-  
+
   ngOnDestroy(): void {
     this.abortRecording();
   }
@@ -151,180 +158,183 @@ export class ValidationComponent implements OnInit {
     link.click();
   }
 
- // Propriété pour stocker l'ID de la frame d'animation pour la progress barre
- private animationFrameId: number | undefined;
+  private updateProgress() {
+    // Demandez à mettre à jour la progression de la barre de progression lors de la prochaine frame d'animation
+    this.animationFrameId = requestAnimationFrame(() => {
+      if (this.currentlyPlayingAudio) {
+        const currentTime = this.currentlyPlayingAudio.currentTime;
+        const duration = this.currentlyPlayingAudio.duration;
 
-    private updateProgress() {
-      // Demandez à mettre à jour la progression de la barre de progression lors de la prochaine frame d'animation
-      this.animationFrameId = requestAnimationFrame(() => {
-        if (this.currentlyPlayingAudio) {
-          const currentTime = this.currentlyPlayingAudio.currentTime;
-          const duration = this.currentlyPlayingAudio.duration;
-    
-          if (!isNaN(duration) && duration > 0) {
-            // Mise à jour la barre de progression 
-            const progress = (currentTime / duration) * 100;
-    
-            // Continue à mettre à jour la progression
-            this.updateProgress();
-          } else {
-            // Arrêt de la mise à jour de la progression si la lecture est terminée ou si la durée n'est pas valide
-            this.animationFrameId = undefined;
-          }
+        if (!isNaN(duration) && duration > 0) {
+          // Mise à jour la barre de progression
+          const progress = (currentTime / duration) * 100;
+
+          // Continue à mettre à jour la progression
+          this.updateProgress();
         } else {
-          // Si l'audio est null, arrêtez la mise à jour de la progression
+          // Arrêt de la mise à jour de la progression si la lecture est terminée ou si la durée n'est pas valide
           this.animationFrameId = undefined;
         }
+      } else {
+        // Si l'audio est null, arrêtez la mise à jour de la progression
+        this.animationFrameId = undefined;
+      }
+    });
+  }
+
+  playAudio(audioUrl: string) {
+    if (
+      this.currentlyPlayingAudio &&
+      this.currentlyPlayingAudio.src === audioUrl
+    ) {
+      // Si le même audio est en cours de lecture, reprendre la lecture
+      if (this.currentlyPlayingAudio.paused) {
+        this.isAudioPlaying[audioUrl] = true; // Réglez l'état de lecture à true
+        this.currentlyPlayingAudio.play();
+      } else {
+        this.currentlyPlayingAudio.pause();
+        this.isAudioPlaying[audioUrl] = false; // Réglez l'état de lecture à false
+      }
+    } else {
+      // Arrêtez l'audio en cours de lecture, le cas échéant
+      if (this.currentlyPlayingAudio) {
+        this.currentlyPlayingAudio.pause();
+        this.isAudioPlaying[this.currentlyPlayingAudio.src] = false;
+      }
+
+      // Jouez le nouvel audio
+      const audio = new Audio(audioUrl);
+
+      // Mise à jour de la progression dès le début de la lecture
+      audio.addEventListener("loadedmetadata", () => {
+        this.updateProgress();
+      });
+
+      audio.play();
+      this.currentlyPlayingAudio = audio;
+      this.isAudioPlaying[audio.src] = true;
+
+      //  gestionnaire d'événement pour l'événement "ended" pour réinitialiser l'audio
+      this.currentlyPlayingAudio.addEventListener("ended", () => {
+        this.isAudioPlaying[audio.src] = false;
+        this.currentlyPlayingAudio = null;
       });
     }
-    
-
-    playAudio(audioUrl: string) {
-      if (this.currentlyPlayingAudio && this.currentlyPlayingAudio.src === audioUrl) {
-        // Si le même audio est en cours de lecture, reprendre la lecture
-        if (this.currentlyPlayingAudio.paused) {
-          this.isAudioPlaying[audioUrl] = true; // Réglez l'état de lecture à true
-          this.currentlyPlayingAudio.play();
-        } else {
-          this.currentlyPlayingAudio.pause();
-          this.isAudioPlaying[audioUrl] = false; // Réglez l'état de lecture à false
-        }
-      } else {
-        // Arrêtez l'audio en cours de lecture, le cas échéant
-        if (this.currentlyPlayingAudio) {
-          this.currentlyPlayingAudio.pause();
-          this.isAudioPlaying[this.currentlyPlayingAudio.src] = false;
-        }
-    
-        // Jouez le nouvel audio
-        const audio = new Audio(audioUrl);
-    
-        // Mise à jour de la progression dès le début de la lecture
-        audio.addEventListener('loadedmetadata', () => {
-          this.updateProgress();
-        });
-    
-        audio.play();
-        this.currentlyPlayingAudio = audio;
-        this.isAudioPlaying[audio.src] = true;
-    
-        //  gestionnaire d'événement pour l'événement "ended" pour réinitialiser l'audio
-        this.currentlyPlayingAudio.addEventListener('ended', () => {
-          this.isAudioPlaying[audio.src] = false;
-          this.currentlyPlayingAudio = null;
-        });
-      }
-    }
-
-
-pauseAudio() {
-  if (this.currentlyPlayingAudio) {
-    // Mémorise la position de lecture actuelle
-    const currentTime = this.currentlyPlayingAudio.currentTime;
-    this.currentlyPlayingAudio.pause();
-    this.isAudioPlaying[this.currentlyPlayingAudio.src] = false;
-
-    // Rétablie la position de lecture lorsque la lecture est reprise
-    this.currentlyPlayingAudio.currentTime = currentTime;
   }
-}
 
-// fonction pour supprimer l'audio mais aussi le container de l'audio
-removeAudio(index: number) {
-  if (index >= 0 && index < this.audioList.length) {
-    // Obtenez l'URL de l'audio à supprimer
-    const audioUrlToRemove = this.audioList[index].url;
-
-    // Arrête l'audio en cours de lecture s'il s'agit de l'audio en cours
-    if (this.currentlyPlayingAudio && this.currentlyPlayingAudio.src === audioUrlToRemove) {
-      this.currentlyPlayingAudio.pause();
-      this.currentlyPlayingAudio = null;
-    }
-
-    // Supprimez l'audio de la liste
-    this.audioList.splice(index, 1);
-  }
-}
-
-
-
-// tableau pour stocker la vitesse de lecture de chaque élément audio
-audioPlaybackRates: { [key: string]: number } = {};
-//fonction pour augmenter la vitesse de lecture
-speedUpAudio(audio: AudioforTransit): void {
-  if (audio) {
-    const audioSrc = audio.url;
-    if (audioSrc) {
-      // Vérifie si l'audio est actuellement en cours de lecture
-      if (this.currentlyPlayingAudio && this.currentlyPlayingAudio.src === audioSrc) {
-        // Augmente progressivement la vitesse de lecture
-        if (this.audioPlaybackRates[audioSrc] === undefined) {
-          this.audioPlaybackRates[audioSrc] = 1.0;
-        } else if (this.audioPlaybackRates[audioSrc] === 1.0) {
-          this.audioPlaybackRates[audioSrc] = 1.5;
-        } else if (this.audioPlaybackRates[audioSrc] === 1.5) {
-          this.audioPlaybackRates[audioSrc] = 2.0;
-        } else {
-          this.audioPlaybackRates[audioSrc] = 1.0;
-        }
-
-        // Applique la nouvelle vitesse de lecture à l'audio en cours
-        this.currentlyPlayingAudio.playbackRate = this.audioPlaybackRates[audioSrc];
-        // Ajoute un gestionnaire d'événement pour l'événement "ended" pour réinitialiser l'audio
-        this.currentlyPlayingAudio.addEventListener('ended', () => {
-          this.isAudioPlaying[audioSrc] = false;
-          this.currentlyPlayingAudio = null;
-          this.audioPlaybackRates[audioSrc] = 1.0;
-        });
-      }
-    }
-  }
-}
-
-//fonction de calcul pour la barre de progression 
-calculateProgress(index: number): number {
-  if (
-    index >= 0 &&
-    index < this.audioList.length &&
-    this.audioList[index] &&
-    this.currentlyPlayingAudio &&
-    this.currentlyPlayingAudio.src === this.audioList[index].url
-  ) {
-    // Vérifie si la durée est disponible et n'est ni null ni Infinity
-    const duration = this.currentlyPlayingAudio.duration;
-    if (duration !== null && duration !== Infinity && !isNaN(duration)) {
+  pauseAudio() {
+    if (this.currentlyPlayingAudio) {
+      // Mémorise la position de lecture actuelle
       const currentTime = this.currentlyPlayingAudio.currentTime;
-      const progress = (currentTime / duration) * 100;
+      this.currentlyPlayingAudio.pause();
+      this.isAudioPlaying[this.currentlyPlayingAudio.src] = false;
 
-      return progress;
+      // Rétablie la position de lecture lorsque la lecture est reprise
+      this.currentlyPlayingAudio.currentTime = currentTime;
     }
   }
 
-  return 0;
-}
+  // fonction pour supprimer l'audio mais aussi le container de l'audio
+  removeAudio(index: number) {
+    if (index >= 0 && index < this.audioList.length) {
+      // Obtenez l'URL de l'audio à supprimer
+      const audioUrlToRemove = this.audioList[index].url;
 
+      // Arrête l'audio en cours de lecture s'il s'agit de l'audio en cours
+      if (
+        this.currentlyPlayingAudio &&
+        this.currentlyPlayingAudio.src === audioUrlToRemove
+      ) {
+        this.currentlyPlayingAudio.pause();
+        this.currentlyPlayingAudio = null;
+      }
+
+      // Supprimez l'audio de la liste
+      this.audioList.splice(index, 1);
+    }
+  }
+
+  //fonction pour augmenter la vitesse de lecture
+  speedUpAudio(audio: AudioforTransit): void {
+    if (audio) {
+      const audioSrc = audio.url;
+      if (audioSrc) {
+        // Vérifie si l'audio est actuellement en cours de lecture
+        if (
+          this.currentlyPlayingAudio &&
+          this.currentlyPlayingAudio.src === audioSrc
+        ) {
+          // Augmente progressivement la vitesse de lecture
+          if (this.audioPlaybackRates[audioSrc] === undefined) {
+            this.audioPlaybackRates[audioSrc] = 1.0;
+          } else if (this.audioPlaybackRates[audioSrc] === 1.0) {
+            this.audioPlaybackRates[audioSrc] = 1.5;
+          } else if (this.audioPlaybackRates[audioSrc] === 1.5) {
+            this.audioPlaybackRates[audioSrc] = 2.0;
+          } else {
+            this.audioPlaybackRates[audioSrc] = 1.0;
+          }
+
+          // Applique la nouvelle vitesse de lecture à l'audio en cours
+          this.currentlyPlayingAudio.playbackRate =
+            this.audioPlaybackRates[audioSrc];
+          // Ajoute un gestionnaire d'événement pour l'événement "ended" pour réinitialiser l'audio
+          this.currentlyPlayingAudio.addEventListener("ended", () => {
+            this.isAudioPlaying[audioSrc] = false;
+            this.currentlyPlayingAudio = null;
+            this.audioPlaybackRates[audioSrc] = 1.0;
+          });
+        }
+      }
+    }
+  }
+
+  //fonction de calcul pour la barre de progression
+  calculateProgress(index: number): number {
+    if (
+      index >= 0 &&
+      index < this.audioList.length &&
+      this.audioList[index] &&
+      this.currentlyPlayingAudio &&
+      this.currentlyPlayingAudio.src === this.audioList[index].url
+    ) {
+      // Vérifie si la durée est disponible et n'est ni null ni Infinity
+      const duration = this.currentlyPlayingAudio.duration;
+      if (duration !== null && duration !== Infinity && !isNaN(duration)) {
+        const currentTime = this.currentlyPlayingAudio.currentTime;
+        const progress = (currentTime / duration) * 100;
+
+        return progress;
+      }
+    }
+
+    return 0;
+  }
 
   delay(ms: number) {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
+
   async addRecordToRecords() {
-    //j'ai honte de faire ca mais les promesses et les observables font n'imp 
+    //j'ai honte de faire ca mais les promesses et les observables font n'imp
     this.stopRecording();
     await this.delay(250);
     const url = window.URL.createObjectURL(this.teste.blob);
-    var audioForTransit = new AudioforTransit;
+    var audioForTransit = new AudioforTransit();
     audioForTransit.url = url;
     audioForTransit.numberVerse = this.currentVerse;
     audioForTransit.duration = this.recordedTime;
     this.audioList?.push(audioForTransit);
     this.currentVerse++;
   }
+
   getArrayOfSize(n: number): any[] {
     return new Array(n);
   }
 }
-class AudioforTransit{
+
+class AudioforTransit {
   numberVerse: number | undefined;
   url: string | undefined;
-  duration: string |undefined;
+  duration: string | undefined;
 }
